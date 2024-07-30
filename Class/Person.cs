a56c4e2c7a4e747f82fdcb5167ac61bf01303aa3
Ahmet Sulu <ahmet.sulu1993@gmail.com>
2024-07-30 18:06:10 +0300

    Add project files.

---
+++ Class/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        // Tam adını almak için method.
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        // Tam yaşı almak için method.
+        public int GetAge()
+        {
+            int age = DateTime.Now.Year - BirthDate.Year;
+            if       (DateTime.Now.DayOfYear < BirthDate.DayOfYear)
+                age -= 1;
+
+            return age;
+        }
+
+        // Bilgileri yazdırmak için method.
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {GetFullName()}");
+            Console.WriteLine($"Birth Date: {BirthDate.ToShortDateString()}");
+            Console.WriteLine($"Age: {GetAge()}");
+        }
+    }
+    }
+   
---
+++ Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using Class;
+
+// Öğrenci bilgilerini oluşturur.
+Person student1 = new Person();
+student1.FirstName = "Ali";
+student1.LastName = "Ayhan";
+student1.BirthDate = new DateTime(2006, 12, 19);
+
+Person student2 = new Person();
+student2.FirstName = "Şeyda";
+student2.LastName = "Kara";
+student2.BirthDate = new DateTime(2006, 12, 28);
+
+
+// Öğretmen bilgilerini oluşturur.
+Person teacher1 = new Person();
+teacher1.FirstName = "Ahmet";
+teacher1.LastName = "Sulu";
+teacher1.BirthDate = new DateTime(1993, 8, 9);
+
+Person teacher2 = new Person();
+teacher2.FirstName = "Nil";
+teacher2.LastName = "Ekşi";
+teacher2.BirthDate = new DateTime(1991, 1, 7);
+
+
+// Öğrenci bilgilerini yazdırır.
+Console.WriteLine("Students:");
+student1.PrintDetails();
+Console.WriteLine();
+student2.PrintDetails();
+Console.WriteLine();
+
+
+// Öğretmen bilgilerini yazdırır.
+Console.WriteLine("Teachers:");
+teacher1.PrintDetails();
+Console.WriteLine();
+teacher2.PrintDetails();
